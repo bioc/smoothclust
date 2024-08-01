0b41fff4abbc1f0e52ffb7b1ac5e827fef0a36a2
Lukas Weber <lmweberedu@gmail.com>
2024-08-01 13:47:29 -0400

    fix test - error due to no neighbors

--- tests/testthat/test_non_SPE_inputs.R
+++ tests/testthat/test_non_SPE_inputs.R
@@ -4,8 +4,9 @@ input <- matrix(rpois(2000, 10), ncol = 100)
 spatial_coords <- matrix(runif(200), ncol = 2)
 
 # run smoothclust with non-SpatialExperiment inputs
+# note: using large bandwidth due to small number of points
 set.seed(123)
-out <- smoothclust(input, spatial_coords = spatial_coords)
+out <- smoothclust(input, spatial_coords = spatial_coords, bandwidth = 0.25)
 
 
 test_that("smoothclust runs with non-SPE inputs", {
